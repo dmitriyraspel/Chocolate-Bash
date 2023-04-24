a771056be98416476709e569d562dc81aa440d96
dmitriy.raspel <dmitriyraspel@yandex.ru>
2023-04-24 21:27:20 +0300

    add template filter-tags.php + styles

---
+++ template-parts/filter-tags.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Template part for displaying filter Tags
+ *
+ * @package rspl_base
+ */
+?>
+
+<div class="filter-tags">
+    <span class="filter-tag__label">SEARCH BY:</span>
+    <ul class="filter-tag__list">
+        <li class="test">
+            <a href="" class="test">CREPE</a>
+        </li>
+        <li class="test">
+            <a href="" class="test">PANCAKE</a>
+        </li>
+        <li class="test">
+            <a href="" class="test">WAFFLE</a>
+        </li>
+        <li class="test">
+            <a href="" class="test">ICE CREAM</a>
+        </li>
+        <li class="test">
+            <a href="" class="test">FRUITS</a>
+        </li>
+        <li class="test">
+            <a href="" class="test">HOT DRINK</a>
+        </li>
+        <li class="test">
+            <a href="" class="test">COLD DRINK</a>
+        </li>
+    </ul>
+</div><!-- /.filter-tag -->
